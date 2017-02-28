9d9d58c00c5fb179ef2c8c49420e7d8d62a98fc4
Sourav Chakraborty <souravchakrabortysrm@gmail.com>
2017-02-28 05:40:22 +0530

    Add files via upload

---
+++ min1.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctime>
+int max, min;
+int a[1000000];
+void maxmin(int i, int j)
+{
+ int max1, min1, mid;
+ if(i==j)
+ {
+  max = min = a[i];
+ }
+ else
+ {
+  if(i == j-1)
+  {
+   if(a[i] <a[j])
+   {
+    max = a[j];
+    min = a[i];
+   }
+   else
+   {
+    max = a[i];
+    min = a[j];
+   }
+  }
+  else
+  {
+   mid = (i+j)/2;
+   maxmin(i, mid);
+   max1 = max; min1 = min;
+   maxmin(mid+1, j);
+   if(max <max1)
+    max = max1;
+   if(min > min1)
+    min = min1;
+  }
+ }
+}
+int main ()
+{ float t1=(float)clock();
+ int i, num;
+ printf ("\nEnter the total number of numbers : ");
+ scanf ("%d",&num);
+ printf ("generating random numbers : \n");
+ for (i=1;i<=num;i++)
+  a[i]=rand();
+
+  printf("printing the numbers \n");
+  for (i=1;i<=num;i++)
+  printf("%d\n",a[i]);
+
+ max = a[0];
+ min = a[0];
+
+ maxmin(1, num);
+
+
+ printf ("Minimum element in an array : %d\n", min);
+ printf ("Maximum element in an array : %d\n", max);
+ float t2=(float)clock();
+
+
+printf("the time taken : %d",(float)t2);
+ return 0;
+}
+
